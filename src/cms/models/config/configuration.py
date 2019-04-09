35336e64b099707cbf553243985807f77ec1fed0
Thomas Weizenegger <thomas.weiz97@gmail.com>
2019-04-09 12:45:14 +0200

    Add federation models

--- src/cms/models/config/configuration.py
+++ src/cms/models/config/configuration.py
@@ -13,7 +13,7 @@ class Configuration(models.Model):
     """
 
     key = models.CharField(max_length=100, unique=True, blank=False)
-    value = models.CharField(max_length=1000, blank=False)
+    value = models.TextField(blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
